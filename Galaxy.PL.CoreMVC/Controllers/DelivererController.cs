59d5eba66a7c5fd5f19820f3612dfcd5cf4158e7
Derebeyi <6437707+batuhanyus@users.noreply.github.com>
2021-11-02 18:59:33 +0300

    Save.

--- Galaxy.PL.CoreMVC/Controllers/DelivererController.cs
+++ Galaxy.PL.CoreMVC/Controllers/DelivererController.cs
@@ -2,15 +2,148 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Galaxy.BusinessLogic.Abstract;
+using Galaxy.Entities;
+using Galaxy.PL.CoreMVC.Models.ViewModels.Deliverer;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Galaxy.PL.CoreMVC.Controllers
 {
     public class DelivererController : Controller
     {
+        IOrderService orderService;
+        IOrderDetailsService orderDetailsService;
+        IUserService userService;
+
+        public DelivererController(IOrderService ordService, IOrderDetailsService ordetService, IUserService usrService)
+        {
+            orderService = ordService;
+            orderDetailsService = ordetService;
+            userService = usrService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("GetDeliveries");
         }
+
+        [Route("Deliverer/GetDeliveries")]
+        public IActionResult GetDeliveries()
+        {
+            List<DelivererOrderViewModel> model = new();
+            ICollection<Order> orders = orderService.GetAll();
+
+            foreach (Order order in orders)
+            {
+                model.Add(new DelivererOrderViewModel()
+                {
+                    CityID = order.CityID,
+                    CountyID = order.CountyID,
+                    DelivererID = order.DelivererID,
+                    MemberID = order.MemberID,
+                    OrderID = order.ID,
+                    OrderStatus = order.OrderStatus,
+                    PackagerID = order.PackagerID,
+                    Details = orderDetailsService.GetOrderDetailsByOrderID(order.ID).ToList()
+                });
+            }
+
+            return View("DelivererOrderList", model);
+        }
+
+        [Route("Deliverer/GetDetails")]
+        public IActionResult GetDetails(int orderID)
+        {
+            Order order = orderService.GetByID(orderID);
+            DelivererOrderViewModel model = CreateModelFromOrder(order);
+
+            return View("DelivererOrderDetails", model);
+        }
+
+        [HttpPost]
+        [Route("Deliverer/UpdateDelivery")]
+        public IActionResult UpdateDelivery(DelivererOrderViewModel model)
+        {
+            Order old = orderService.GetByID(model.OrderID);
+            Order young = CreateOrderFromModel(model);
+            orderService.Update(old, young);
+
+            return RedirectToAction("GetDetails", new { model.OrderID });
+        }
+
+        List<SelectListItem> CreateDelivererList()
+        {
+            ICollection<User> dbDeliverers = userService.GetAllDeliverers();
+            List<SelectListItem> deliverers = new();
+
+            foreach (User deliverer in dbDeliverers)
+            {
+                deliverers.Add(new SelectListItem()
+                {
+                    Value = deliverer.ID.ToString(),
+                    Text = deliverer.Name + " " + deliverer.Surname
+                });
+            }
+
+            return deliverers;
+        }
+
+        Order CreateOrderFromModel(DelivererOrderViewModel model)
+        {
+            return new Order()
+            {
+                ID = model.OrderID,
+                CityID = model.CityID,
+                CountyID = model.CountyID,
+                DelivererID = model.DelivererID,
+                MemberID = model.MemberID,
+                OrderStatus = model.OrderStatus,
+                PackagerID = model.PackagerID
+            };
+        }
+
+        DelivererOrderViewModel CreateModelFromOrder(Order order)
+        {
+            return new DelivererOrderViewModel()
+            {
+                PackagerID = order.PackagerID,
+                OrderID = order.ID,
+                OrderStatus = order.OrderStatus,
+                MemberID = order.MemberID,
+                DelivererID = order.DelivererID,
+                CountyID = order.CountyID,
+                CityID = order.CityID,
+                Deliverers = CreateDelivererList(),
+                OrderStatuses = CreateOrderStatuses(),
+                Details = orderDetailsService.GetOrderDetailsByOrderID(order.ID).ToList()
+            };
+        }
+
+        private List<SelectListItem> CreateOrderStatuses()
+        {
+            List<SelectListItem> statuses = new();
+
+            statuses.Add(new SelectListItem()
+            {
+                Value = "Delivered",
+                Text = "Delivered"
+            });
+
+            statuses.Add(new SelectListItem()
+            {
+                Value = "Incorrect Address",
+                Text = "Incorrect Address"
+            });
+
+            statuses.Add(new SelectListItem()
+            {
+                Value = "Member Was Not Found",
+                Text = "Member Was Not Found"
+            });
+
+            return statuses;
+        }
+
     }
 }
---
+++ Galaxy.PL.CoreMVC/Models/ViewModels/Deliverer/DelivererOrderViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Galaxy.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Galaxy.PL.CoreMVC.Models.ViewModels.Deliverer
+{
+    public class DelivererOrderViewModel
+    {
+        public DelivererOrderViewModel()
+        {
+            Details = new();
+        }
+
+        public int OrderID { get; set; }
+        public int MemberID { get; set; }
+        public int PackagerID { get; set; }
+        public int DelivererID { get; set; }
+        public int CityID { get; set; }
+        public int CountyID { get; set; }
+        
+
+        public string OrderStatus { get; set; }
+
+        public List<OrderDetails> Details { get; set; }
+        public List<SelectListItem> Deliverers { get; set; }
+        public List<SelectListItem> OrderStatuses { get; set; }
+    }
+}
